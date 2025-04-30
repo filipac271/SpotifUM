938e535e0629ba39260e50a02f167a3d7d731a07
a107329 <a107329@alunos.uminho.pt>
2025-04-30 21:23:59 +0100

    Adicao de doxygen nas pastas de Album, PlanoSubscricao e Playlist

--- SpotifUM/src/main/java/PlanoSubscricao/PlanoPremiumBase.java
+++ SpotifUM/src/main/java/PlanoSubscricao/PlanoPremiumBase.java
@@ -1,16 +1,30 @@
 package PlanoSubscricao;
 
+/**
+ * Representa o plano de subscrição "Premium Base".
+ * 
+ * Este plano oferece funcionalidades premium básicas como playlists e avanço/retrocesso,
+ * e atribui pontos adicionais simples.
+ */
+public class PlanoPremiumBase extends PlanoPremium {
 
-
-public class PlanoPremiumBase extends PlanoPremium{
-    public PlanoPremiumBase()
-    {  
+    /**
+     * Construtor por omissão.
+     * Inicializa o plano com o nome "Premium Base".
+     */
+    public PlanoPremiumBase() {  
         super();
-         this.nomePlano="Premium Base"; 
-    
+        this.nomePlano = "Premium Base"; 
     }
-    public double calculaPontos(double pontos)
-    {
-        return pontos+10;
+
+    /**
+     * Calcula os pontos atribuídos com base nos pontos atuais.
+     * 
+     * @param pontos Pontos atuais.
+     * @return Pontos atualizados (acrescenta 10).
+     */
+    @Override
+    public double calculaPontos(double pontos) {
+        return pontos + 10;
     }
 }
